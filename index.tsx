9f1472c7f3c81be96ef1c1699ac04b8536e85d7a
精武陈真 <546369005@qq.com>
2021-05-21 22:54:37 +0800

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,16 @@
+/**
+* This is an auto-generated demo by dumi
+* if you think it is not working as expected,
+* please report the issue at
+* https://github.com/umijs/dumi/issues
+**/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
